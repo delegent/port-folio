30b5b18e6cd01d927c55b007c9518a434994c1fd
Anubhav Srivastava <srivastavaanubhav764@gmail.com>
2022-03-25 01:10:10 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const navBtn = document.querySelector('#navbar-toggle');
+const navDiv = document.querySelector('.navbar-collapse');
+
+navBtn.addEventListener('click',()=>{
+    navDiv.classList.toggle('showNav');
+});
+
+
+let resizeTimer;
+window.addEventListener('resize',()=>{
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(()=>{
+        document.body.classList.remove('resize-animation-stopper');},400);
+    })
+let img = document.getElementById('home-img');
+img.addEventListener('mouseover',function(){
+    
+        img.src = "images/colour.jpg"
+    
+})
+img.addEventListener('mouseout',function(){
+    
+    img.src = "images/colour.jpg"
+
+})
+//about
+
+// remember
+// let modeBtn = document.querySelector(".mode-btn");
+// modeBtn.addEventListener('click',function(){
+//     let rootVar = document.querySelector(':root');
+//     rootVar.style.setProperty('--green','yellow');
+//     rootVar.style.setProperty('--black','#fff');
+//     rootVar.style.setProperty('--nero','#e3e3e3');
+//     rootVar.style.setProperty('--text','#111');
+//     rootVar.style.setProperty('--white','black');
+    
+
+
+
+
+//     // --green :#00b7ff;
+//     //  --black: #111;
+//     //  --nero:#252525;
+//     //  --transition:all 0.4s ease;
+//     //  --heading:rgb(58, 58, 58);
+//     //  --text:#d0d0d0;
+//     //  --white:#fff;
+// })
---
+++ scriptn.js
@@ -0,0 +1,64 @@
+const addBtn = document.getElementById('add');
+
+const notes = JSON.parse(localStorage.getItem('notes'));
+
+if(notes){
+	notes.forEach(note => addNewNote(note));
+}
+
+addBtn.addEventListener('click',()=> addNewNote(""));
+function addNewNote(text = ""){
+  const note = document.createElement('div');
+  note.classList.add('note');
+  
+   if(text != ""){
+	note.innerHTML = ' <div class="tools">  <button class="edit"><i class = "fas fa-edit"></i></button> <button class="delete"><i class = "fas fa-trash-alt"></i></button></div><div class = "main"></div> <textarea class = "hidden"></textarea>';
+   }else{
+	note.innerHTML = ' <div class="tools">  <button class="edit"><i class = "fas fa-edit"></i></button> <button class="delete"><i class = "fas fa-trash-alt"></i></button></div><div class = "main hidden"></div> <textarea class = ""></textarea>';
+   }
+
+  const editBtn = note.querySelector('.edit')
+  const deleteBtn = note.querySelector('.delete')
+  const main = note.querySelector('.main')
+  const textArea = note.querySelector('textarea')
+
+  textArea.value = text;
+  main.innerHTML = marked(text)
+
+
+
+   deleteBtn.addEventListener('click',()=>{
+	   note.remove();
+	   updateLS()
+   })
+
+   editBtn.addEventListener('click',()=>{
+	   main.classList.toggle('hidden');
+	   textArea.classList.toggle('hidden');
+   })
+
+textArea.addEventListener('input',(e)=>{
+  const value = e.target.value;
+  main.innerHTML = marked(value);
+  updateLS()
+})
+
+
+ document.body.appendChild(note); 
+}
+
+
+
+// localStorage.setItem('name',JSON.stringify())
+// JSON.parse(localStorage.getItem('name'))
+// localStorage.removeItem('name');
+function updateLS(){
+
+   const notesText = document.querySelectorAll('textarea');
+   const notes = []
+   notesText.forEach(note => notes.push(note.value))
+   console.log(notes);
+   localStorage.setItem('notes',JSON.stringify(notes));
+   
+
+}
