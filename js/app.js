4b1e76c0af7662eeacf876f1fec0818373798b6f
Anubhav Srivastava <srivastavaanubhav764@gmail.com>
2022-04-04 23:24:31 +0530

    Sec. Commit

--- js/app.js
+++ js/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffa600"
+        "value": "#04c5c5"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#ffa600"
+          "color": "#04c5c5"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffa600",
+        "color": "#04c5c5",
         "opacity": 0.4,
         "width": 1
       },
@@ -122,7 +122,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "#ffa600",
+      "background_color": "#04c5c5",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
